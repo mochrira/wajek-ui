ffcd40862aa95755914cbf01d22be127383c8132
Moch Rizal Rachmadani <mochrira@gmail.com>
2020-09-11 13:30:20 +0700

    include all required assets

--- projects/wui-report/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of wui-report
- */
-
-export * from './lib/viewer/viewer.component';
-export * from './lib/wui-report.module';
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ReportComponent } from './pages/report/report.component';
+import { FormsComponent } from './pages/forms/forms.component';
 
 const routes: Routes = [{
+    path: 'forms', component: FormsComponent
+}, {
     path: 'report', component: ReportComponent
 }, {
     path: 'home', component: HomeComponent
