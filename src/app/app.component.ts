1f10c702be95fb465ef98c0d722f0ac414a4d3e0
mochrira <mochrira@gmail.com>
2025-09-27 10:52:20 +0700

    change the scss framework completely

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WuiAppComponent, WuiDrawerComponent, WuiDrawerItemComponent } from '@wajek/wui';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports : [WuiDrawerComponent, WuiDrawerItemComponent, RouterOutlet, WuiAppComponent]
+    imports : []
 })
 export class AppComponent {
 
